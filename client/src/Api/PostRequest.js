a0018f4e0f99c3af28d2b849e4ae546a3bb19e77
ArunGithub23 <garun2161@gmail.com>
2024-12-22 20:02:14 +0530

    changed like to post method from put

--- client/src/Api/PostRequest.js
+++ client/src/Api/PostRequest.js
@@ -3,4 +3,4 @@ import axios from 'axios'
 const API=axios.create({baseURL:"https://socialmediaserver-dqlu.onrender.com"})
 
 export const getTimeLinePosts=(id)=>API.get(`/post/${id}/timeline`)
-export const likePost=(id,userid)=>API.put(`/post/${id}/like`,{userid:userid})
+export const likePost=(id,userid)=>API.post(`/post/${id}/like`,{userid:userid})
